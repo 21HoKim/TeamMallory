1a92bfae72b6488a75419ec823f1776e47abe52d
Ho Kim <86921102+21HoKim@users.noreply.github.com>
2023-10-20 17:12:56 +0900

    코드 수정&주석

--- study/main.c
+++ study/main.c
@@ -19,7 +19,7 @@ Param param = {
 
 bool parse(Param *param, int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc != 3)
     {
         usage();
         return false;
@@ -31,29 +31,34 @@ bool parse(Param *param, int argc, char *argv[])
 int main(int argc, char *argv[])
 {
 
-    if (!parse(&param, argc, argv))
+    if (!parse(&param, argc, argv)) //프로그램 인자값 검사
         return -1;
 
-    unsigned char *Interface = argv[1];
-    unsigned char *AP_MAC = argv[2];
-    unsigned char *STA_MAC = argv[3];
+    unsigned char *Interface = argv[1]; //네트워크 인터페이스
+    unsigned char *AP_MAC = argv[2]; //AP의 Mac Address
 
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *pcap = pcap_open_live(Interface, BUFSIZ, 1, 1000, errbuf);
+    pcap_t *pcap = pcap_open_live(Interface, BUFSIZ, 1, 1000, errbuf); //해당 함수를 사용하면 네트워크 인터페이스와 연결된 포인터를 만든다.
+
     if (pcap == NULL)
     {
-        fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
+        fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf); //에러 메시지 출력
         return -1;
     }
+    while(true){
+      struct pcap_pkthdr* header;
+		  const u_char* packet; //패킷의 첫번째 주소를 가리키는 포인터
+		  int res = pcap_next_ex(pcap, &header, &packet);  //packet에 packet의 주소가 들어감, pcap_next_ex 함수는 다음 패킷을 받는 함수.
+		  if (res == 0) continue;
+		  if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+			  printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+			  break;
+		  }
 
-    /*
-    
-    
-    여기서부터 코드 작성
-    
-    
-    */
+      /*    
+      여기서부터 코드 작성
+      */
 
-
+    }
     pcap_close(pcap);
 }
